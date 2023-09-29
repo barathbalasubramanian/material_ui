eb27add2dc291e381f354f45391ba950a1254051
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-09-29 19:25:52 +0530

    Tools and Tech�

---
+++ src/components/ImageRotation.js
@@ -0,0 +1,57 @@
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Plane, OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+
+const ImageRotator = ({ imagePaths }) => {
+  return (
+    <Canvas camera={{ position: [0, 0, 15] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 1, 1]} />
+      <SceneRotation>
+        {imagePaths.map((path, index) => (
+          <ImagePlane 
+            key={path} 
+            path={path} 
+            index={index} 
+            totalImages={imagePaths.length} 
+          />
+        ))}
+      </SceneRotation>
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+const SceneRotation = ({ children }) => {
+  const groupRef = useRef();
+  
+  useFrame(() => {
+    if (groupRef.current) {
+    //   groupRef.current.rotation.z += 0.05;
+        groupRef.current.rotation.y += 0.05
+    }
+  });
+  
+  return <group ref={groupRef}>{children}</group>;
+};
+
+const ImagePlane = ({ path, index, totalImages, radius = 5 }) => {
+    const texture = new THREE.TextureLoader().load(path);
+    
+    // Positioning images spherically using the Fibonacci Sphere algorithm
+    const phi = Math.acos(1 - 2 * (index + 0.5) / totalImages);
+    const theta = Math.sqrt(totalImages * Math.PI) * phi;
+    
+    return (
+      <Plane args={[1, 1]} position={[
+        Math.cos(theta) * Math.sin(phi) * radius,
+        Math.sin(theta) * Math.sin(phi) * radius,
+        Math.cos(phi) * radius
+      ]}>
+        <meshBasicMaterial attach="material" map={texture} side={THREE.DoubleSide} />
+      </Plane>
+    );
+  };
+  
+export default ImageRotator;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,17 @@
-import React, { useEffect } from 'react'
-import Bg from '../components/bg'
+import React, { useEffect } from 'react';
+import Bg from '../components/bg';
+import ImageRotator from '../components/ImageRotation';
+import vue  from '../assets/icons/vue.png';
+import html from '../assets/icons/html.png';
+import css from '../assets/icons/css.png';
+import js from '../assets/icons/js.png';
+import python from '../assets/icons/py.png';
+import django from '../assets/icons/dj.png'
+import vite from '../assets/icons/vite.png'
 
 function About() {
+
+  const Imagepaths = [  vue , html , css , js , python ,django , vite ]
 
   useEffect(() => {
     document.title = 'About 😊'
@@ -10,6 +20,9 @@ function About() {
   return (
     <div>
       <Bg />
+      <div className="imageRotator">
+        <ImageRotator imagePaths={Imagepaths} />
+      </div>
       <div className="centered-content">
           <p>👋 Hi, I’m Barathkumar</p>
           <p>👀 I’m interested in Web development and ML.</p>
